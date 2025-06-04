0b023e111f04341c29e1aff538d3f06a6b42365f
MBALIBULA JONARD <jonardmbalibula@gmail.com>
2025-06-04 23:01:03 +0300

    Update script.js
    Signed-off-by: MBALIBULA JONARD <jonardmbalibula@gmail.com>

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
+// Array of background image filenames
+const backgroundImages = ['bg1.jpg', 'bg2.jpg', 'bg3.jpg', 'me.webp'];
+let currentIndex = 0;
 
-const images = ['bg1.jpg', 'bg2.jpg', 'bg3.jpg', 'me.webp'];
-let current = 0;
-
+// Function to change the body's background image
 function changeBackground() {
-  document.body.style.backgroundImage = `url('${images[current]}')`;
-  current = (current + 1) % images.length;
+  document.body.style.backgroundImage = `url('${backgroundImages[currentIndex]}')`;
+  currentIndex = (currentIndex + 1) % backgroundImages.length;
 }
 
+// Set the initial background image when the page loads
+changeBackground();
+
+// Change background every 5 seconds
 setInterval(changeBackground, 5000);
